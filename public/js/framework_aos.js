6a7ad758bd58f2399725e2714fd1a5d417e2ab87
jorge <jorge.arivilca@gmail.com>
2021-12-15 23:40:15 -0300

    se agregaron imagenes nuevas para la pagina productos y el framework aos.js para nimaciones

---
+++ public/js/framework_aos.js
@@ -0,0 +1,14 @@
+
+// cofigo para aos.js
+AOS.init({
+    easing: 'ease-out-back',
+    duration: 1000
+});
+
+hljs.initHighlightingOnLoad();
+
+    $('.hero__scroll').on('click', function(e) {
+        $('html, body').animate({
+            scrollTop: $(window).height()
+        }, 1200);
+    });
